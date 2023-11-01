b12731ca46541f458e6ad08f239162395fa29296
impulse <32690185+impulse888@users.noreply.github.com>
2023-11-01 07:50:12 -0700

    Create create_new_extractor.sh

---
+++ create_new_extractor.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+NAME=$1;
+TYPE="extractor";
+
+if [ -z "$NAME" ]; then
+	printf "No plugin name provided.\nUsage: %s <plugin name>\nExample: %s hanime\n" "$0" "$0";
+    exit 1;
+fi
+
+mkdir -p "./pluggables/$NAME/yt_dlp_plugins/$TYPE";
+touch "./pluggables/$NAME/yt_dlp_plugins/$TYPE/$NAME.py";
+printf "Put your %s code in './pluggables/%s/yt_dlp_plugins/%s/%s.py'\n"  "$TYPE" "$NAME" "$TYPE" "$NAME";
+printf "Done.\n";
+exit 0;
